16ab4219ef1afdea160113f6671643d551d72449
RAJI-Zakaria <zakariaraji.me@gmail.com>
2024-06-12 01:54:48 +0200

    skeleton fix

--- app/search/loading.tsx
+++ app/search/loading.tsx
@@ -7,14 +7,14 @@ const Loading = () => {
       <h1 className="text-3xl font-bold mb-2">Searching...</h1>
       <h2 className="mb-5 text-gray-400">We wont be long...</h2>
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-        <Skeleton className="bg-white shadow-md p-5 rounded-lg h-[300px] " />
-        <Skeleton className="bg-white shadow-md p-5 rounded-lg h-[300px] " />
-        <Skeleton className="bg-white shadow-md p-5 rounded-lg h-[300px] " />
-        <Skeleton className="bg-white shadow-md p-5 rounded-lg h-[300px] " />
-        <Skeleton className="bg-white shadow-md p-5 rounded-lg h-[300px] " />
-        <Skeleton className="bg-white shadow-md p-5 rounded-lg h-[300px] " />
-        <Skeleton className="bg-white shadow-md p-5 rounded-lg h-[300px] " />
-        <Skeleton className="bg-white shadow-md p-5 rounded-lg h-[300px] " />
+        <Skeleton className="shadow-md p-5 rounded-lg h-[300px] w-full " />
+        <Skeleton className="shadow-md p-5 rounded-lg h-[300px] w-full " />
+        <Skeleton className="shadow-md p-5 rounded-lg h-[300px] w-full " />
+        <Skeleton className="shadow-md p-5 rounded-lg h-[300px] w-full " />
+        <Skeleton className="shadow-md p-5 rounded-lg h-[300px] w-full " />
+        <Skeleton className="shadow-md p-5 rounded-lg h-[300px] w-full] " />
+        <Skeleton className="shadow-md p-5 rounded-lg h-[300px] w-full " />
+        <Skeleton className="shadow-md p-5 rounded-lg h-[300px] w-full " />
       </ul>
     </div>
   );
